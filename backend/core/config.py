7292bc51054202e7a20b78ad1e33ae535fc84cd1
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2021-10-25 23:56:55 -0500

    config bd

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,22 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
 class Settings:
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
+
+    # Database
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "localhost")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
+    DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
 settings = Settings()
